289989701f1046e0f0911070672c05eeb08a46fa
Cazarin <jose.cazarin@dextra-sw.com>
2019-03-30 21:23:30 -0300

    Better names for tests of question2

--- Question2/TestQuestion2.py
+++ Question2/TestQuestion2.py
@@ -14,7 +14,7 @@ class CheckLinesTest(unittest.TestCase):
     def test_complex_equal(self):
         self.assertEqual(Question2.compare_version_strings("1:9.0.1", "1:9.0.1"), 0)
 
-    def test_equal_complex(self):
+    def test_complex_equal_many_symbols(self):
         self.assertEqual(Question2.compare_version_strings("1;1.1,1;1;1:", "1;1;1;1;1;1,"), 0)
 
     def test_many_subversions_lot_chars(self):
@@ -23,16 +23,16 @@ class CheckLinesTest(unittest.TestCase):
     def test_sanity(self):
         self.assertEqual(Question2.compare_version_strings("1.1", "1.2"), 1)
 
-    def test_second_longer(self):
+    def tests_different_lengths(self):
         self.assertEqual(Question2.compare_version_strings("1.9", "1.10.5"), 1)
 
     def test_dashes(self):
         self.assertEqual(Question2.compare_version_strings("1-9", "1-10"), 1)
 
-    def test_many_subversions_different_lenghts(self):
+    def test_last_one_different(self):
         self.assertEqual(Question2.compare_version_strings("1.9", "1.9.5"), 1)
 
-    def test_big_strange_characters(self):
+    def test_complex_different_many_symbols(self):
         self.assertEqual(Question2.compare_version_strings("1;1;1;1;1;0", "1;1;1;1;1;1"), 1)
 
     def test_many_subversions(self):
@@ -44,10 +44,10 @@ class CheckLinesTest(unittest.TestCase):
     def test_first_one_longer(self):
         self.assertEqual(Question2.compare_version_strings("1.9.5", "1.9"), -1)
 
-    def test_first_sub_bigger(self):
+    def test_first_bigger(self):
         self.assertEqual(Question2.compare_version_strings("2", "1.1.1"), -1)
 
-    def test_first_sub_bigger_with_zeros_fill(self):
+    def test_first_bigger_with_zeros_fill(self):
         self.assertEqual(Question2.compare_version_strings("2.0.0.0.0.0.0.0", "1.1.1"), -1)
 
 
