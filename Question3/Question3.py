47b5145ee5613c0a8145d8572352414f575dba53
Cazarin <jose.cazarin@dextra-sw.com>
2019-03-31 17:36:18 -0300

    Added return values to cache functions Added DEFINES to some values Added more tests Added print functions

--- Question3/Question3.py
+++ Question3/Question3.py
@@ -1,14 +1,19 @@
 from collections import deque
 
+MAX_CACHE_SIZE = 4
+CACHE_HIT_HAPPENED = 0
+CACHE_FULL_DISCARDED_LRU = 1
+ADDED_TO_CACHE = 2
 
-class My_cache:
+
+class MyCache:
     """In this class the MRU (most recently used) is the leftmost element of the deque
     that represents the cache memory.
     That said, the LRU (last recently used) is the rightmost element"""
 
     def __init__(self):
 
-        self.MAX_CACHE_SIZE = 4
+        self.MAX_CACHE_SIZE = MAX_CACHE_SIZE
         self.cache_memory = deque()
         self.hit_count = 0
 
@@ -19,57 +24,32 @@ class My_cache:
             self.cache_memory.remove(element)
             self.cache_memory.appendleft(element)
             self.hit_count = self.hit_count + 1
-            return
+            return CACHE_HIT_HAPPENED
 
         # If it's not present, append to the cache memory
         # If its full, remove the LRU (the rightmost one)
-
         if (len(self.cache_memory) + 1) > self.MAX_CACHE_SIZE:
             self.cache_memory.pop()
             self.cache_memory.appendleft(element)
+            return CACHE_FULL_DISCARDED_LRU
 
         else:
             self.cache_memory.appendleft(element)
+            return ADDED_TO_CACHE
 
     def print_hit_count(self):
-        print("hit count = " + str(self.hit_count))
+        print("Hit Count = {}".format(self.hit_count))
+
+    def print_cache_memory_content(self):
+        print("Cache Memory Contains = {} ".
+              format(self.return_cache_content_as_list()))
 
     def return_cache_content_as_list(self):
         return list(self.cache_memory)
 
-    def print_cache_content(self):
-        print("cache content = " + str(self.cache_memory))
+    def return_hit_count(self):
+        return self.hit_count
 
 
 if __name__ == '__main__':
-    cache = My_cache()
-
-    cache.put_in_cache(1)
-    cache.put_in_cache(2)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(1)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(3)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(1)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(4)
-    cache.put_in_cache(5)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(5)
-
-    cache.print_cache_content()
-
-    cache.put_in_cache(3)
-
-    cache.print_cache_content()
+    pass
--- Question3/TestQuestion3.py
+++ Question3/TestQuestion3.py
@@ -9,12 +9,126 @@ import unittest
 class MemoryCacheTests(unittest.TestCase):
 
     def setUp(self):
-        self.test_cache = Question3.My_cache()
+        self.test_cache = Question3.MyCache()
 
     def test_put_1_in_cache(self):
         mock_cache = [1]
-        self.test_cache.put_in_cache(1)
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+
+        # Check if the cache content is as expected
         self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 0)
+
+    def test_put_1_and_2_in_cache(self):
+        mock_cache = [2, 1]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 0)
+
+    def test_moving_cache_hit_to_front(self):
+        mock_cache = [1, 3, 2]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(3), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 1)
+
+    def test_cache_limit(self):
+        mock_cache = [5, 4, 3, 2]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(3), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(4), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(5), Question3.CACHE_FULL_DISCARDED_LRU)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 0)
+
+    def test_cache_limit_and_moving_to_front(self):
+        mock_cache = [1, 4, 3, 2]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(3), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(4), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 1)
+
+    def test_many_cache_hits(self):
+        mock_cache = [1]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 5)
+
+    def test_many_cache_hits_two_values(self):
+        mock_cache = [2, 1]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 9)
+
+    def test_many_cache_hits_three_values(self):
+        mock_cache = [3, 2, 1]
+
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(1), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(2), Question3.CACHE_HIT_HAPPENED)
+        self.assertEqual(self.test_cache.put_in_cache(3), Question3.ADDED_TO_CACHE)
+        self.assertEqual(self.test_cache.put_in_cache(3), Question3.CACHE_HIT_HAPPENED)
+
+        # Check if the cache content is as expected
+        self.assertListEqual(self.test_cache.return_cache_content_as_list(), mock_cache)
+
+        self.assertEqual(self.test_cache.return_hit_count(), 10)
 
 
 if __name__ == '__main__':
